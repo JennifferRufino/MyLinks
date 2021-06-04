78d5c0fc6dbde2eb7a42b4327b420ea0bfc36663
unknown <guerreiramaya2016@gmail.com>
2021-06-04 16:33:09 -0300

    Aula 02 dinalizada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Modal} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 
 import {LinearGradient} from 'expo-linear-gradient';
 import StatusBarPage from '../../components/StatusBarPage';
 import Menu from '../../components/Menu';
+import ModalLink from '../../components/ModalLink';
 
 import {
     ContainerLogo, 
@@ -19,36 +21,62 @@ import {
 } from './styles';
 
 export default function Home(){
+
+    const [input, setInput] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
+
+    function handleShortLink() {
+        //alert("Ola," + input);
+
+        setModalVisible(true);
+    }
+
     return(
-        <LinearGradient
-            colors={['#1ddbb9', '#132742']}
-            style={{flex:1, justifyContent: 'center'}}
-        >
-            <StatusBarPage barStyle = 'light-content' backgroundColor = "#1ddbb9" />
-            <Menu />
-            <ContainerLogo>
-                <Logo source={require('../../assets/Logo.png')} resizeMode="contain" />
-            </ContainerLogo>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <LinearGradient
+                colors={['#1ddbb9', '#132742']}
+                style={{flex:1, justifyContent: 'center'}}
+            >
+                <StatusBarPage barStyle = 'light-content' backgroundColor = "#1ddbb9" />
+                <Menu />
+                <KeyboardAvoidingView
+                    behavior= {Platform.OS === 'android' ? 'padding' : 'position'}
+                    enabled
+                >
+                    <ContainerLogo>
+                        <Logo source={require('../../assets/Logo.png')} resizeMode="contain" />
+                    </ContainerLogo>
 
-            <ContainerContent>
-                <Title>SujeitoLink</Title>
-                <SubTitle>Cole seu link para encurtar</SubTitle>
-            </ContainerContent>
-            <ContainerInput>
-                <BoxIcon>
-                    <Feather name="link" size={22} color="#FFF" />
-                </BoxIcon>
-                <Input 
-                    placeholderTextColor = "white"
-                    placeholder="Cole seu link aqui..."     
-                />
-            </ContainerInput>
+                    <ContainerContent>
+                        <Title>Encurta URL</Title>
+                        <SubTitle>Cole seu link para encurtar</SubTitle>
+                    </ContainerContent>
+                </KeyboardAvoidingView>
+                <ContainerInput>
+                    <BoxIcon>
+                        <Feather name="link" size={22} color="#FFF" />
+                    </BoxIcon>
+                    <Input 
+                        placeholderTextColor = "white"
+                        placeholder="Cole seu link aqui..." 
+                        autoCapitalize="none"
+                        autoCorrect ={false}
+                        keyboardType = "url"
+                        value={input}
+                        onChangeText = {(text) => setInput(text)}
+                    />
+                </ContainerInput>
 
-            <ButtonLink>
-                <ButtonLinkText>
-                    Gerar Link
-                </ButtonLinkText>
-            </ButtonLink>
-        </LinearGradient>
+                <ButtonLink onPress={handleShortLink}>
+                    <ButtonLinkText>
+                        Gerar Link
+                    </ButtonLinkText>
+                </ButtonLink>
+
+                <Modal visible={modalVisible} transparent animationType="slide">
+                    <ModalLink onClose={() => setModalVisible(false)} />
+                </Modal>
+            </LinearGradient>
+        </TouchableWithoutFeedback>
     );
 }
